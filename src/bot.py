a0381cfe5c6b7d042cbe36a664d4c20821316019
qiaoyi213 <linqiaoyi213@gmail.com>
2024-04-05 20:06:28 +0800

    feat: Implement basic command to test the bot

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,26 @@
 import discord
-from util import get_discord_token
+from util import get_discord_token, get_openai_api_key
 from discord.ext import commands
+from openai_controller import openai_controller
 
 intents = discord.Intents.all()
-
-bot = commands.Bot(command_prefix='[', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
+GAI = openai_controller(get_openai_api_key())
 
 @bot.event
 async def on_ready():
     print(">> Bot is ready <<")
+
+@bot.command(name="ping")
+async def ping(ctx):
+
+    await ctx.send("pong")
+
+@bot.command()
+async def test(ctx, arg1, arg2):
+    await ctx.send(f'You passed {arg1} and {arg2}')
+
+
 
 bot.run(get_discord_token())
 
--- src/util.py
+++ src/util.py
@@ -3,3 +3,6 @@ def get_discord_token():
     token = json.load(open('../token.json'))["discord-token"]
     return token
 
+def get_openai_api_key():
+    token = json.load(open('../token.json'))["OPENAI_API_KEY"]
+    return token
